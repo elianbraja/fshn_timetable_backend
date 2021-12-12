8414efa91297559e9bd8871a598d357d5007ca55
elianbraja <elianbraja@yahoo.com>
2021-12-12 23:06:29 +0100

    root path updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get '/', :to => 'creators#show'
   get 'creator', :to => 'creators#show'
   get 'timetable/student', :to => 'timetable#student'
   get 'timetable/pedagog', :to => 'timetable#pedagog'
